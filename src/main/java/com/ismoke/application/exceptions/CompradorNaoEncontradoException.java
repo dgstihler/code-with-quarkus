41964225ac7a64e8ecb83a05184da841109d7c53
Stihler <diogo.galdino@philips.com>
2024-12-08 22:53:12 -0300

    [tests] create test comprador

--- src/main/java/com/ismoke/application/exceptions/CompradorNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.ismoke.application.exceptions;
-
-public class CompradorNaoEncontradoException extends RuntimeException {
-    public CompradorNaoEncontradoException(String cpfComprador) {
-        super("Comprador com o cpf" + cpfComprador + " não cadastrado/encontrado.");
-    }
-}
--- src/main/java/com/ismoke/application/exceptions/VendedorNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.ismoke.application.exceptions;
-
-public class VendedorNaoEncontradoException extends RuntimeException {
-    public VendedorNaoEncontradoException(String cnpjVendor) {
-        super("Vendedor com o cnpj" + cnpjVendor + " não cadastrado/encontrado.");
-    }
-}
